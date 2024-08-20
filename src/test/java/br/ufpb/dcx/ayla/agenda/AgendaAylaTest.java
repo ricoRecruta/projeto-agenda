31c47978d87ae10c9ebbba272f4de982af778f0d
Joao <marketting@letscode.com.br>
2024-08-20 09:47:01 -0300

    códigos do programa agenda

--- src/test/java/br/ufpb/dcx/ayla/agenda/AgendaAylaTest.java
+++ src/test/java/br/ufpb/dcx/ayla/agenda/AgendaAylaTest.java
@@ -3,6 +3,7 @@ package br.ufpb.dcx.ayla.agenda;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,7 +11,7 @@ public class AgendaAylaTest {
 
     @Test
     public void testaAgendaAyla(){
-        Agenda a1 = new AgendaAyla();
+       /* Agenda a1 = new AgendaAyla();
         assertTrue(a1.cadastraContato("Gabriela", 12,04));
         assertEquals(1 ,a1.pesquisaAniversariantes(12,04).size());
         try{
@@ -18,6 +19,32 @@ public class AgendaAylaTest {
             fail();
         }catch (ContatoInexistenteException e){
             //ok
-        }
+        } */
+
+        AgendaAyla agenda  = new AgendaAyla();
+        agenda.pesquisaAniversariantes(20,8);
+        Collection<Contato> contatosAchados = agenda.pesquisaAniversariantes(20,8);
+
+        assertTrue(contatosAchados.size() == 0);
+        agenda.cadastraContato("Maria",20,8);
+        agenda.cadastraContato("José",20,8);
+
+        contatosAchados = agenda.pesquisaAniversariantes(20,8);
+
+        assertEquals(2,contatosAchados.size());
+        Contato maria = new Contato("Maria",20,8);
+        assertTrue(contatosAchados.contains(maria));
+        Contato jose = new Contato("José",20,8);
+        assertTrue(contatosAchados.contains(jose));
+
+        try {
+            boolean removeu = agenda.removeContato("José");
+            assertTrue(removeu);
+        }catch(ContatoInexistenteException e){ fail("lançou excessão quando não deveria");}
+
+
+
+
+
     }
 }
